a8edec204301a2da349d7543ce3458b472033e11
sertnayong <nuannimprasert@gmail.com>
2025-01-19 22:43:52 +0700

    b3

--- app/api/insertValues/route.ts
+++ app/api/insertValues/route.ts
@@ -1,23 +1,23 @@
 import { NextResponse } from 'next/server';
-//import { Pool } from 'pg';
+import { Pool } from 'pg';
 
 // Create a connection pool
-// const pool = new Pool({
-//   connectionString: process.env.POSTGRES_URL,
-// });
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+});
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
     const { temperature, humidity } = body;
 
- return NextResponse.json({ success: true, message: temperature, humidity });
+ return NextResponse.json({ success: true, message:humidity, humidity });
     if (!temperature || !humidity) {
       return NextResponse.json({ error: 'Temperature and humidity are required!' }, { status: 400 });
     }
 
-   // const query = 'INSERT INTO sensor_data (temperature, humidity, timestamp) VALUES ($1, $2, NOW())';
-    //await pool.query(query, [temperature, humidity]);
+    const query = 'INSERT INTO sensor_data (temperature, humidity, timestamp) VALUES ($1, $2, NOW())';
+    await pool.query(query, [temperature, humidity]);
 
     return NextResponse.json({ success: true, message: 'Data inserted successfully!' });
   } catch (error) {
